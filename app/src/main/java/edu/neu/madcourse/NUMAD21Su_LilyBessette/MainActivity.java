73f883d5ca3f2aa0ec1edb2f1a90ec06eb5e731e
Lily Bessette <lilyguibessette@gmail.com>
2021-05-10 21:19:58 -0400

    init repo CS5520

---
+++ app/src/main/java/edu/neu/madcourse/NUMAD21Su_LilyBessette/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.NUMAD21Su_LilyBessette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
